cf747dadd21b888bed5ccf3b2cdac750abc0f6ef
Mashopy <eliasgheeraert@gmail.com>
2025-07-25 04:15:01 +0200

    packages: Introduce NothingFelicaDisabler
    The purpose of this entire app is to disable FeliCa packages for non-JPN SKUs after Google
    did [1] in Android 16 and non-JPN SKUs does not support FeliCa at all due to a different NFC hardware
    (ex: non-JPN SKUs use ST21NFC while JPN SKU use ST54J/ST54L with eSE support on most Nothing devices).
    
    [1]: https://android.googlesource.com/platform/frameworks/base/+/26ab55038d82a6b9ff4d9080a3c8ab94eaa92e1c
    
    Co-authored-by: Cyber Knight <cyberknight755@gmail.com>
    Change-Id: I4fae64efe051a7868f2925f8a01920f30d83589c

---
+++ packages/FelicaDisabler/src/org/lineageos/felica/FelicaDisabler.kt
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2025 The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.lineageos.felica
+
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.SystemProperties
+import android.util.Log
+
+object FelicaDisabler {
+    private const val TAG = "NothingFelicaDisabler"
+
+    private val FELICA_PACKAGES =
+        listOf(
+            "com.felicanetworks.mfm.main",
+            "com.felicanetworks.mfs",
+            "com.felicanetworks.mfc",
+            "com.felicanetworks.mfw.a.boot",
+        )
+
+    fun enableOrDisableFelica(context: Context) {
+        val pm = context.packageManager
+        val sku = SystemProperties.get("ro.boot.hardware.sku", "")
+        val isJapaneseVariant = sku == "JPN"
+        val flag =
+            if (isJapaneseVariant) PackageManager.COMPONENT_ENABLED_STATE_ENABLED
+            else PackageManager.COMPONENT_ENABLED_STATE_DISABLED
+
+        for (pkg in FELICA_PACKAGES) {
+            try {
+                pm.setApplicationEnabledSetting(pkg, flag, 0)
+            } catch (e: Exception) {
+                Log.w(TAG, "Failed to change state for $pkg: ${e.message}")
+            }
+        }
+    }
+}
