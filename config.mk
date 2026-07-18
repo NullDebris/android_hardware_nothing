fb704180435656a7bf8a272ebafb0232eef7b1d8
hiroshi. <hiroshi@tanuki.page>
2026-07-17 22:42:56 -0400

    sepolicy: Import policy for eSimSwitcher

--- config.mk
+++ config.mk
@@ -3,3 +3,8 @@ HW_NOTHING_PATH = hardware/nothing
 PRODUCT_SOONG_NAMESPACES += $(HW_NOTHING_PATH)
 
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(HW_NOTHING_PATH)/sepolicy/common/private
+
+ifneq ($(filter NothingEsimSwitcher ,$(PRODUCT_PACKAGES)),)
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(HW_NOTHING_PATH)/sepolicy/esimswitcher/private
+endif
+
