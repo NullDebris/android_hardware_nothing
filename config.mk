3b6337e0e1ed127f1a3537153a2aba5778dfc4a5
hiroshi. <hiroshi@tanuki.page>
2026-07-18 10:49:55 -0400

    sepolicy: Import policy for DeviceExtras
    vendor side is kept in device trees in case of differing labels

--- config.mk
+++ config.mk
@@ -8,3 +8,7 @@ ifneq ($(filter NothingEsimSwitcher ,$(PRODUCT_PACKAGES)),)
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(HW_NOTHING_PATH)/sepolicy/esimswitcher/private
 endif
 
+ifneq ($(filter DeviceExtras ,$(PRODUCT_PACKAGES)),)
+SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(HW_NOTHING_PATH)/sepolicy/DeviceExtras/private
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(HW_NOTHING_PATH)/sepolicy/DeviceExtras/public
+endif
