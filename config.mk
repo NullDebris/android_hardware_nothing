24619dc683b9c061b847beefb15ffd9615c24eea
hiroshi. <hiroshi@tanuki.page>
2026-07-29 02:34:18 -0400

    sepolicy: Import contexts for fingerprint/sensors services

--- config.mk
+++ config.mk
@@ -3,6 +3,7 @@ HW_NOTHING_PATH = hardware/nothing
 PRODUCT_SOONG_NAMESPACES += $(HW_NOTHING_PATH)
 
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(HW_NOTHING_PATH)/sepolicy/common/private
+BOARD_VENDOR_SEPOLICY_DIRS += $(HW_NOTHING_PATH)/sepolicy/common/vendor
 
 ifneq ($(filter NothingEsimSwitcher ,$(PRODUCT_PACKAGES)),)
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(HW_NOTHING_PATH)/sepolicy/esimswitcher/private
