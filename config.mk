6c67ca1c695704af896f492498070d85c10d586c
chandu078 <chandudyavanapelli03@gmail.com>
2026-07-29 03:11:26 -0400

    sepolicy: Import policies for sltntc

--- config.mk
+++ config.mk
@@ -4,6 +4,7 @@ PRODUCT_SOONG_NAMESPACES += $(HW_NOTHING_PATH)
 
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(HW_NOTHING_PATH)/sepolicy/common/private
 BOARD_VENDOR_SEPOLICY_DIRS += $(HW_NOTHING_PATH)/sepolicy/common/vendor
+SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(HW_NOTHING_PATH)/sepolicy/common/public
 
 ifneq ($(filter NothingEsimSwitcher ,$(PRODUCT_PACKAGES)),)
 SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(HW_NOTHING_PATH)/sepolicy/esimswitcher/private
